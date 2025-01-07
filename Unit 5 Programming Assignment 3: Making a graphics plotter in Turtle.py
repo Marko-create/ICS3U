bb0857825dc571d8f57a354db625de1eff35f8d0
Marko-create <753888@pdsb.net>
2025-01-07 10:55:40 -0500

    Update Unit 5 Programming Assignment 3: Making a graphics plotter in Turtle.py

--- Unit 5 Programming Assignment 3: Making a graphics plotter in Turtle.py
+++ Unit 5 Programming Assignment 3: Making a graphics plotter in Turtle.py
@@ -1,12 +1,23 @@
-import turtle
-
-colors = 4
-
-for i in range(colors)
-c_info = fh.readline()
-[sym, c, colorstr] = c_info.split()
-
-if sym = "~":
-    sym = " "
-
-colorDiet[sym] = colorStr
+file = "smiley_emoji_mod.xpm"
+try:
+    fh = open(file, "r")
+    line2 = fh.readline().strip()
+    [cols, rows, numColors] = line2.split(" ")
+    num2 = int(cols)
+    num3 = int(rows)
+    numColors = int(numColors)
+    print(num2)
+    print(num3)
+    print(numColors)
+    symbols = {}
+    for i in range(numColors):
+        line = fh.readline().strip()
+        [sym, c, color] = line.split(" ")
+        if sym == "~":
+            sym = " "
+        symbols[sym] = color
+    print(symbols)
+    r = fh.readline().strip()
+except OSError as err:
+    print("Not found", err)
+fh.close()
